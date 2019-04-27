5095159d040b9274611145328f3fc83d456b994a
Alexander Voronkov <yorkbc2@gmail.com>
2019-04-27 21:34:07 +0300

    Server added

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express"),
+  app = express(),
+  path = require("path"),
+  fs = require("fs");
+
+app.use(express.static(path.resolve(__dirname, "dist")));
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  const index = fs.readFileSync(
+    path.resolve(__dirname, "dist", "index.html"),
+    "utf-8"
+  );
+  res
+    .status(200)
+    .send(index)
+    .end();
+});
+
+app.listen(process.env.PORT || 3100);
